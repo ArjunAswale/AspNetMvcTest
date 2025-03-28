e44c058f06e8e07bada87e095baf8f46f4acfc58
ArjunAswale <144764220+ArjunAswale@users.noreply.github.com>
2025-03-28 13:27:05 +0530

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,22 @@
+﻿using MachineTest_NimapTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MachineTest_NimapTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int pageIndex = 1, int pageSize = 10)
+        {
+            var products = _context.Products.Include(p => p.Category).AsQueryable();
+            var paginatedProducts = await PaginatedList<Product>.CreateAsync(products, pageIndex, pageSize);
+            return View(paginatedProducts);
+        }
+    }
+}
