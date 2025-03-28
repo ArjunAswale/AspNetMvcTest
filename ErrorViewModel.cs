63c6cb0bb5cfb0e4eb828f8e9e5a5fd0382ce827
ArjunAswale <144764220+ArjunAswale@users.noreply.github.com>
2025-03-28 13:37:10 +0530

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MachineTest_NimapTask.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
